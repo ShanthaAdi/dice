f0a07928b826ee2a7c9b69f7c2699c4259612208
ShanthaAdi <72916481+ShanthaAdi@users.noreply.github.com>
2023-03-25 14:27:22 +0530

    images

---
+++ images/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+    var img1 = document.getElementsByClassName("img1");
+//    img1.setAttribute("src","./images/dice"+num1+".png");
+//                                             // "./images/dice1.png"
+
+//     console.log(img1_attri);
